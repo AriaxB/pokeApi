c7aee98729463f3e4ec0ab11fdecf02bfe322b8b
jhonyx <jnba0529@gmail.com>
2024-07-12 22:16:41 -0500

    arreglo de error en una imagen

--- pokeApi/src/frontend/titulo.jsx
+++ pokeApi/src/frontend/titulo.jsx
@@ -4,7 +4,7 @@ import '../styles/index.css'
 export default function Title(){
     return(
         <div className="title-container">
-            <img src="https://fontmeme.com/temporary/63aedff9efd74176a82d50b547cc79ba.png" alt="pokemon" />
+            <img src="https://fontmeme.com/permalink/240713/7ab94b3b6e8b9b0c5e59a0d7502f562f.png" alt="pokemon" />
         </div>
     )
 }
